427ba074c7c84f49b9138b7c674d31dd2b8ae8e6
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2023-12-04 21:22:37 -0300

    Multiple CMakes test

---
+++ Engine/src/Application.cpp
@@ -0,0 +1,10 @@
+//
+// Created by luizh on 04/12/2023.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, Engine!" << std::endl;
+    return 0;
+}
---
+++ SandBox/src/Application.cpp
@@ -0,0 +1,10 @@
+//
+// Created by luizh on 04/12/2023.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, SandBox!" << std::endl;
+    return 0;
+}
